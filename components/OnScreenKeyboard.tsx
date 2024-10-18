f647eb67d9033e2b0e2bd9abaedd8f7468c29005
mateopolci <mateopolci@hotmail.com>
2024-10-18 18:03:39 -0300

    basic keyboard ui

--- components/OnScreenKeyboard.tsx
+++ components/OnScreenKeyboard.tsx
@@ -1,14 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+    Pressable,
+    StyleSheet,
+    Text,
+    useWindowDimensions,
+    View,
+} from "react-native";
+import React from "react";
 
-const OnScreenKeyboard = () => {
-  return (
-    <View>
-      <Text>OnScreenKeyboard</Text>
-    </View>
-  )
-}
+type OnScreenKeyboardProps = {
+    onKeyPressed: (key: string) => void;
+    blueLetters: string[];
+    yellowLetters: string[];
+    grayLetters: string[];
+};
 
-export default OnScreenKeyboard
+export const ENTER = "ENTER";
+export const BACKSPACE = "BACKSPACE";
 
-const styles = StyleSheet.create({})
+const keys = [
+    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+    ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+    [ENTER, "z", "x", "c", "v", "b", "n", "m", BACKSPACE],
+];
+
+const OnScreenKeyboard = ({
+    onKeyPressed,
+    blueLetters,
+    yellowLetters,
+    grayLetters,
+}: OnScreenKeyboardProps) => {
+    const { width } = useWindowDimensions();
+    const keyWidth = (width - 60) / keys[0].length;
+    const keyHeight = 60;
+
+    const isSpecialKey = (key: string) => [ENTER, BACKSPACE].includes(key);
+
+    const isInLetters = (key: string) =>
+        [...blueLetters, ...yellowLetters, ...grayLetters].includes(key);
+
+    return (
+        <View style={styles.container}>
+            {keys.map((row, rowIndex) => (
+                <View key={`row-${rowIndex}`} style={styles.row}>
+                    {row.map((key, keyIndex) => (
+                        <Pressable
+                            key={`key=${key}`}
+                            onPress={() => onKeyPressed(key)}
+                            style={[styles.key, { width: keyWidth, height: keyHeight, backgroundColor: '#DDD' }]}
+                        >
+                            <Text style={styles.keyText}>{key}</Text>
+                        </Pressable>
+                    ))}
+                </View>
+            ))}
+        </View>
+    );
+};
+
+export default OnScreenKeyboard;
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 40,
+        gap: 4,
+        alignSelf: "center",
+    },
+    row: {
+        flexDirection: "row",
+        gap: 4,
+        justifyContent: "center",
+    },
+    key: {
+		alignItems: "center",
+		justifyContent: "center",
+		borderRadius: 4,
+	},
+    keyText: {
+        fontSize: 20,
+        fontWeight: "bold",
+		textTransform: "uppercase",
+    },
+});
